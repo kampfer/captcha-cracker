4ddecc99e57e747a3fe6ffaebea04de1364a1639
liaowei02 <liaowei02@baidu.com>
2017-09-08 18:00:43 +0800

    update

---
+++ download_captcha.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+import json
+import base64
+import time
+
+def downloadCtripCaptcha():
+    url = 'https://accounts.ctrip.com/member/ajax/AjaxChkBWGAndVerifyCode.ashx?username=18701439558&st=sgo'
+    data = json.loads(urllib.urlopen(url).read())
+    imgData = base64.b64decode(data['Image'])
+    fn = str(time.time()) + '.jpg'
+    fw = open(fn, 'wb')
+    fw.write(imgData)
+    fw.close()
+    return fn
+
+for i in range(100):
+    print(i, downloadCtripCaptcha())
+    time.sleep(1)
